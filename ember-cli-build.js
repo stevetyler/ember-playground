9ce7e01b6a0e865e663c431e1022efa52181cc8e
Steve Tyler <steve.tyler75@gmail.com>
2021-05-14 17:59:20 +0100

    added routes

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,12 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    prember: {
+      urls: [
+        '/',
+        '/my-account'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
