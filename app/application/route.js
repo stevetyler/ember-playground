aec40d3a5805195778d84f511f73128ff8ca9224
Steve Tyler <steve.tyler75@gmail.com>
2021-05-17 14:29:01 +0100

    updated images

--- app/application/route.js
+++ app/application/route.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
-  // redirect() {
-  //   this.transitionTo('my-account');
-  // }
+  redirect() {
+    this.transitionTo('my-account');
+  }
 }
